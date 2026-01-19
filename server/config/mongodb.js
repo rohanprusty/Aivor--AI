1b894ad62740559df899bac72b85bbdcc9b8de15
rohanprusty <rohankrprusty@gmail.com>
2026-01-19 18:59:39 +0530

    new changes

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "aivor_ai",
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
